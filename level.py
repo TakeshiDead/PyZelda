4ef913efc29875acadc16fe8ddc8ffbf9772bf57
Caryrl <jatnabatista@gmail.com>
2022-12-12 11:35:57 -0500

    Level

--- level.py
+++ level.py
@@ -10,8 +10,14 @@ class YSortCameraGroup(pygame.sprite.Group):
 
         def custom_draw(self, player):
 
+            #getting the offset
             self.offset.x = player.rect.centerx - self.half_width
             self.offset.y = player.rect.centery - self.half_height
+
+            #drawing the floor
+
+            floor_offset_pos = self.floor_pos = self.floor_rect.topleft = self.offset
+            self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
             for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
                 offset_pos = sprite.rect.topleft - self.offset
