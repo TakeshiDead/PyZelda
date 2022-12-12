eeb712f1616d733ff5738d39d2b0aec443b43988
Alex O Pena Gomez <oniel212@hotmail.com>
2022-12-12 16:23:50 -0500

    creation of the Weapons data

---
+++ settings.py
@@ -0,0 +1,15 @@
+# game setup
+WIDTH = 1280
+HEIGHT = 720
+FPS = 60
+TILESIZE = 64
+
+# Weapons
+weapon_data = {
+'sword':{'cooldown':100,'damage':15,'graphic':''},
+'lance':{'cooldown':400,'damage':30,'graphic':''},
+'axe':{'cooldown':300,'damage':20,'graphic':''},
+'rapier':{'cooldown':50,'damage':8,'graphic':''},
+'sai':{'cooldown':80,'damage':10,'graphic':''},
+
+}
