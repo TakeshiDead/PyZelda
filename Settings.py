79f31cf210a243d4da9f32a40c02137dfa1c2392
Alex O Pena Gomez <oniel212@hotmail.com>
2022-12-12 17:11:26 -0500

    Creation of the Weapons methods and functions

---
+++ Settings.py
@@ -0,0 +1,15 @@
+# game setup
+WIDTH = 1280
+HEIGHT = 720
+FPS = 60
+TILESIZE = 64
+
+# Weapons
+weapon_data = {
+'sword':{'cooldown':100,'damage':15,'graphic':'PATH OF THE SWORD HERE'},
+'lance':{'cooldown':400,'damage':30,'graphic':'PATH OF THE LANCE HERE'},
+'axe':{'cooldown':300,'damage':20,'graphic':'PATH OF THE AXE HERE'},
+'rapier':{'cooldown':50,'damage':8,'graphic':'PATH OF THE RAPIER HERE'},
+'sai':{'cooldown':80,'damage':10,'graphic':'PATH OF THE SAI HERE'},
+
+}
--- Weapon.py
+++ Weapon.py
@@ -3,4 +3,18 @@ import pygame
 class Weapon(pygame.sprite.Sprite):
     def _init_(self,player,groups):
     super()._init_(groups)
-    self.image = pygame.Surface((40,40))
+    direction = player.status.split('_')[0]
+
+    # graphic
+    full_path = f'PATH OF THE WEAPONS HERE{player.weapon}/{direction}.png'
+    self.image = pygame.image.loud(full_path).convert_alpha()
+
+    #placement
+    if direction == 'right':
+        self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(0,16))
+    elif direction == 'left':
+        self.rect = self.image.get_rect(midright = player.rect.midleft + pygame.math.Vector2(0,16))
+    elif direction == 'down':
+         self.rect = self.image.get_rect(midtop = player.rect.midbottom + pygame.math.Vector2(-10,0))
+    else:
+        self.rect = self.image.get_rect(midbottom = player.rect.midtop + pygame.math.Vector2(-10,0))
