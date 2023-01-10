f704cfb7e4dca75ca8d9843b2ecc436c2bd8da51
Caryrl <jatnabatista@gmail.com>
2023-01-09 22:25:51 -0500

    Level

--- level.py
+++ level.py
@@ -1,26 +1,31 @@
-import pygame, sys
+import pygame
 from settings import *
+from tile import Tile
+from player import Player
+from debug import debug
 
-class YSortCameraGroup(pygame.sprite.Group):
+class Level:
     def __init__(self):
-        super().__init__()
+
         self.display_surface = pygame.display.get_surface()
-        self.half_width = self.display_surface.get_size()[0] // 2
-        self.half_height = self.display_surface.get_size()[1] // 2
-        self.offset = pygame.math.Vector2()
 
-        self.floor_surface = pygame.image.load('PyZelda\level\level 0\level 0_Tile Layer 1.csv')
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
 
-        def custom_draw(self, player):
-            # getting the offset
-            self.offset.x = player.rect.centerx - self.half_width
-            self.offset.y = player.rect.centery - self.half_height
 
-            # drawing the floor
 
-            floor_offset_pos = self.floor_pos = self.floor_rect.topleft = self.offset
-            self.display_surface.blit(self.floor_surf, floor_offset_pos)
+    def create_mat(self):
+        for row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    self.player = Player((x,y), [self.visible_sprites])
 
-            for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
-                offset_pos = sprite.rect.topleft - self.offset
-                self.display_surface.blt(sprite.image, offset_pos),
+    def run(self):
+
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
