2568648d4a836b4d7d5fae47d112df558e7a0a99
Alex O Pena Gomez <oniel212@hotmail.com>
2023-01-16 12:51:17 -0500

    merge

--- tile.py
+++ tile.py
@@ -1,15 +1,13 @@
 import pygame
 from settings import *
 
-class Tile(pygame.sprite.Sprite):
-<<<<<<< HEAD
-    def __init__(self, pos, groups):
-        super().__init__(groups)
-        self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
+class Title(pygame.sprite.Sprite):
+    def _init_(self,pos,groups,sprite_type,surface = pygame.Surface((TILESIZE,TILESIZE)))
+    super()._init_(groups)
+    self.sprite_type = sprite_type
+    self.image = surface
+    if Sprite_type =='object':
+        self.rect = self.image.get_rect(topleft = (pos[0],pos[1]- TILESIZE))
+    else:
         self.rect = self.image.get_rect(topleft = pos)
-=======
-    def __init__(self,pos,groups):
-        super().__init__(groups)
-        self.image = pygame.image.load('graphics/test/rock.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
->>>>>>> Caryrl-main
+        self.hitbox = self.rect.inflate(0,-10)
